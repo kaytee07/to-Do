a8e594a34f0b3012dbaaccf45088459e9774a878
kaytee07 <kofi.taylor07@gmail.com>
2022-04-09 04:51:56 +0000

    setting up ui skeleton

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ttf$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        include: path.resolve(
+          __dirname,
+          "./node_modules/bootstrap-icons/font/fonts"
+        ),
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "webfonts",
+            publicPath: "../webfonts",
+          },
+        },
+      },
+    ],
+  },
+};
